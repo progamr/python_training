a97053c9f1776898a44ce25bc181e6fa9cf66279
progamr <prog.amr2014@gmail.com>
2015-12-10 17:51:23 +0200

    syntax

--- syntax.py
+++ syntax.py
@@ -1,13 +1,13 @@
+# this script shoes the importance of the main Function
 
 
 def main():
-    print('this is main function')
+    print('this is the main function')
+    myfunc()
 
 
 def myfunc():
-    print('this is my func')
+    print('this is my function i will call it before i define it!')
+
 if __name__ == "__main__":
     main()
-
-
-
